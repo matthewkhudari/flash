ad36fde6cdb7eeb89cfa1c2e35f332aeb9b88873
Matthew Khudari <mfk611@gmail.com>
2020-09-28 01:40:49 -0400

    Get flask secret key from environment variable

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.environ['FLASH_SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flash.db'
 
 db = SQLAlchemy(app)
